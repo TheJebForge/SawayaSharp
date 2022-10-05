2dcecce87c15ca19061b8e3b942608b30417a841
TheJebForge <jeb@thejebforge.com>
2022-10-06 02:12:20 +0300

    Lowered default volume even more

--- src/Modules/PlayerModule.cs
+++ src/Modules/PlayerModule.cs
@@ -345,7 +345,7 @@ public class PlayerModule: InteractionModuleBase
 
             if (user!.VoiceChannel != null) {
                 player = await _audio.JoinAsync<QueuedLavalinkPlayer>(Context.Guild.Id, user.VoiceChannel.Id, true);
-                await player.SetVolumeAsync(0.5f);
+                await player.SetVolumeAsync(0.1f);
             }
             else {
                 await RespondAsync(_locale["resp.player.novoicechannel"]);
